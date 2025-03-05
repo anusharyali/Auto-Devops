b97c4776d99d38be44623338a30addd04a2dfa49
anusha <porapuanusha@gmail.com>
2025-03-05 10:40:18 +0000

    Ticket 12345-modification

--- Transaction.sh
+++ Transaction.sh
@@ -1,3 +1,14 @@
+=============================================================================================================================
+Fork Mechanism: 
+============================================================================================================================
+A fork is a copy of a repository. Fork is mechanism for creating an duplicate of repository into non restricted account.
+A Fork allows you to freely experiment the changes without effecting the original account.
+A Fork is performed only once in the beginning.
+=============================================================================================================================
+Pull Request
+=============================================================================================================================
+Pull requests let you tell others about changes you've pushed to a branch in a repository on GitHub. Once a pull request is opened, you can discuss and review the potential changes with collaborators and add follow-up commits before your changes are merged into the base branch.
+
 
 [root@ip-172-31-14-178 aws-automation-repo]# git reset --mixed c71821e
 Unstaged changes after reset:
